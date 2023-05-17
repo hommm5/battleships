3fb923bd16f816b63e7a2ff07267af0521d939a1
hommm5 <hommm5@gmail.com>
2023-05-17 14:30:29 +0300

    login

--- src/main/java/com/example/battle_ships_app/controller/HomeController.java
+++ src/main/java/com/example/battle_ships_app/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.example.battle_ships_app.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
 
     @GetMapping("/home")
